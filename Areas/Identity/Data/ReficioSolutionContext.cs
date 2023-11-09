35a5732c718fb88066b30349dbb1293c27e94cd7
TonyLe02 <tonynl@student.uia.no>
2023-11-09 20:46:08 +0100

    Initial Commit

---
+++ Areas/Identity/Data/ReficioSolutionContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration; // Add this namespace
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using ReficioSolution.Areas.Identity.Data;
+
+namespace ReficioSolution.Data
+{
+    public class ReficioSolutionContext : IdentityDbContext<ReficioSolutionUser>
+    {
+        private readonly IConfiguration _configuration; // Add a field to store the configuration
+
+        public ReficioSolutionContext(DbContextOptions<ReficioSolutionContext> options, IConfiguration configuration)
+            : base(options)
+        {
+            _configuration = configuration; // Initialize the configuration
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Get the connection string from appsettings.json using the IConfiguration
+                var connectionString = _configuration.GetConnectionString("ReficioSolutionContextConnection");
+
+                // Configure the database connection
+                optionsBuilder.UseMySql(connectionString, new MariaDbServerVersion(new Version(10, 5, 12)));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+using Microsoft.Extensions.Configuration;
+using ReficioSolution.Areas.Identity.Data;
+using ReficioSolution.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("ReficioSolutionContextConnection") ?? throw new InvalidOperationException("Connection string 'ReficioSolutionContextConnection' not found.");
+
+builder.Services.AddDbContext<ReficioSolutionContext>(options =>
+{
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 0))); // Provide a valid MySQL version
+});
+
+builder.Services.AddDefaultIdentity<ReficioSolutionUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ReficioSolutionContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios.
+    // See: https://aka.ms/aspnetcore-hsts
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
