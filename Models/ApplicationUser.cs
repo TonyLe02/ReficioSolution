a155b54c839b38061308e5798aeaf6469625a4a0
TonyLe02 <tonynl@student.uia.no>
2023-11-10 15:12:29 +0100

    Update ApplicationUser.cs

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,7 +4,7 @@ namespace ReficioSolution.Models
 {
     public class ApplicationUser: IdentityUser
     {
-        public string FirstName {  get; set; }
-        public string LastName { get; set; }
+        public string Firstname {  get; set; }
+        public string Lastname { get; set; }
     }
 }
